bbb00455c0e1aefaefa83afeddd8702d520415a6
Utkarsh Singh <69565691+realutkarshh@users.noreply.github.com>
2026-03-18 19:07:54 +0530

    fix/validation-done

--- validation.js
+++ validation.js
@@ -5,3 +5,6 @@ function validateEmail(email) {
 function validatePassword(password) {
     return password.length >= 6;
 }
+
+console.log("Did some bug fixing on this file but it is not yet completed");
+console.log("Now I will try to go back to the main branch but git won't allow me to do that");
