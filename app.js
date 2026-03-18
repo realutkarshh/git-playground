b77ca79071f5af9005322949637ffe27c7532fe2
Utkarsh Singh <69565691+realutkarshh@users.noreply.github.com>
2026-03-18 18:54:08 +0530

    fix: reset done

--- app.js
+++ app.js
@@ -11,3 +11,4 @@ console.log("Main updated before rebase");
 console.log("Line 1 to study reset");
 console.log("Line 2 to study reset");
 console.log("Line 3 to study reset");
+console.log("Line 4 to study reset - reset done");
